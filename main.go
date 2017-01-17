12fb9e657514276e58b998697fcc00e775c372e0
Eben Freeman <freemaneben@gmail.com>
2017-01-17 10:55:20 -0800

    Tweak.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"sync"
 	"sync/atomic"
 
@@ -18,6 +19,7 @@ type Options struct {
 	Mix         float64 `long:"mix" default:"0.5" description:"fraction of writes"`
 	Concurrency int64   `long:"concurrency" default:"64" description:"concurrency"`
 	Ops         int64   `long:"ops" default:"100000" description:"maximum ops"`
+	Silent      bool    `long:"silent" description:"Suppress stdout"`
 }
 
 func main() {
@@ -28,15 +30,20 @@ func main() {
 func parseFlags() Options {
 	var options Options
 	flagParser := flag.NewParser(&options, flag.Default)
-	flagParser.Parse()
+	_, err := flagParser.Parse()
+	if err != nil {
+		fmt.Println("Error parsing options", err)
+		os.Exit(1)
+	}
 	return options
 }
 
 func genDoc(key int64) map[string]interface{} {
 	doc := map[string]interface{}{
-		"key":  key,
-		"name": "Bananarama",
-		"data": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"key":   key,
+		"name":  "Test",
+		"data":  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		"count": 123,
 	}
 	return doc
 }
@@ -44,10 +51,16 @@ func genDoc(key int64) map[string]interface{} {
 func run(options Options) error {
 	s, err := mgo.Dial(options.Host)
 	if err != nil {
+		fmt.Println("Error dialing Mongo host", err)
 		return err
 	}
 	db := s.DB(options.DBName)
 	c := db.C(options.Collection)
+	err = c.EnsureIndex(mgo.Index{Key: []string{"key"}})
+	if err != nil {
+		fmt.Println("Error creating index", err)
+		return err
+	}
 	var counter int64
 	var insertCounter int64 = 1
 	var wg sync.WaitGroup
@@ -57,22 +70,28 @@ func run(options Options) error {
 		go worker(c, &counter, &insertCounter, &wg, options)
 	}
 	wg.Wait()
+	fmt.Printf("Executed %d ops (%d inserts)\n", counter, insertCounter)
 	return nil
 }
 
 func worker(c *mgo.Collection, counter *int64, insertCounter *int64, wg *sync.WaitGroup, options Options) {
 	for atomic.LoadInt64(counter) < options.Ops {
 		op := rand.Float64() < options.Mix
+		var opName string
 		if op {
 			key := atomic.AddInt64(insertCounter, 1)
 			c.Insert(genDoc(key))
+			opName = "insert"
 		} else {
 			key := rand.Intn(int(atomic.LoadInt64(insertCounter)))
-			c.Find(bson.M{"key": key})
-
+			res := bson.M{}
+			c.Find(bson.M{"key": key}).One(&res)
+			opName = "find"
 		}
 		v := atomic.AddInt64(counter, 1)
-		fmt.Println(v)
+		if !options.Silent {
+			fmt.Println(v, opName)
+		}
 	}
 	wg.Done()
 }
